f60179bda3ab26d3fb538011936c982063d7d341
pacho <pacho2390@gmail.com>
2023-08-29 18:19:23 -0500

    ajustes en js

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
 
 //codigo del footer
 const botonConctacto = document.getElementById('botonContacto');
-// const listaDesplegableContacto = document.getElementById('listaDesplegableContacto-footer');
-
+const listaDesplegableContacto = document.getElementById('listaDesplegableContacto-footer');
 botonConctacto.addEventListener('click', desplegarMenuContacto);
 
 function desplegarMenuContacto(){
-    // console.log('funciona');
     
-    // listaDesplegableContacto.appendChild(abrirMenuContacto)
+       
+    if(document.querySelector('.menuContacto')){
+
+        document.querySelector('.menuContacto').classList.add('inactive');
+        // abrirMenuContacto.removeChild('div')
+
+    }else {
+        
+        const abrirMenuContacto = document.createElement('div');
+        abrirMenuContacto.classList.add('menuContacto');
+        
+        let celular = document.createElement('h3')
+        celular.textContent ="cel :0000000000"
+        abrirMenuContacto.appendChild(celular)
+        
+
+        let correo = document.createElement('h3')
+        correo.textContent ="Correo electrónico:garritasfelinas@mail.com"
+        abrirMenuContacto.appendChild(correo)
+        
+
+        let escribenos = document.createElement('h3')
+        escribenos.textContent = "escribenos"
+        abrirMenuContacto.appendChild(escribenos)
+        
+
+        listaDesplegableContacto.appendChild(abrirMenuContacto)
+        listaDesplegableContacto.classList.remove('');
+        
+                
+    }
+
+}    
+
     
-    const abrirMenuContacto = document.createElement('div');
-    abrirMenuContacto.classList.add('menuContacto');
 
-    let celular = document.createElement('h3')
-    celular.textContent ="cel :0000000000"
-    abrirMenuContacto.appendChild(celular)
-
-    let correo = document.createElement('h3')
-    correo.textContent ="Correo electrónico:garritasfelinas@mail.com"
-    abrirMenuContacto.appendChild(correo)
-
-    let escribenos = document.createElement('h3')
-    escribenos.textContent = "escribenos"
-    abrirMenuContacto.appendChild(escribenos)
-
-    
-
-    // listaDesplegableContacto.classList.remove('inactive
     // let celular = document.createElement('h3')
     // celular.textContent ="cel: 0000000000"
     // listaDesplegableContacto.appendChild(celular)
@@ -35,8 +49,6 @@ function desplegarMenuContacto(){
     // let correo = document.createElement('h3')
     // correo.textContent ="Correo electrónico:garritasfelinas@mail.com"
     // listaDesplegableContacto.appendChild(correo)
-
     // let escribenos = document.createElement('h3')
     // escribenos.textContent = "escribenos"
     // listaDesplegableContacto.appendChild(escribenos)
-}
